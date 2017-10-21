6c23d9f69be8a4453e72c2e0f42d3ee30cfab1ae
bagyabagss <32951402+bagyabagss@users.noreply.github.com>
2017-10-21 09:51:25 +0530

    Add files via upload

---
+++ python_addsub/.adder.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece004
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece004 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=10
+b=20
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
