9d2c626b1918195af7e1dd94c5dd8a84fac9cfef
bagyabagss <32951402+bagyabagss@users.noreply.github.com>
2017-10-21 12:16:31 +0530

    Add files via upload

---
+++ python_addsub/while.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece004
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece004 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+count = 0
+while (count < 6):
+   print 'hello:', count
+   count = count + 1
+print "Good bye!"
