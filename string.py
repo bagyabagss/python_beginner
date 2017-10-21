48d577cba55799bcef717d33be5f3d4c0826f1e4
bagyabagss <32951402+bagyabagss@users.noreply.github.com>
2017-10-21 10:57:31 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece004
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece004 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+      pass
+
+if __name__ == '__main__':
+    main()
+mystring='hi from guvi' + 'test'
+print(mystring[1:3])
+print(mystring.split(" "))
+print(mystring[-1])
+print(mystring.find("u"))
