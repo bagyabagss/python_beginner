9f6b73b1426a772aa1578a596ce51472fa2d83e1
bagyabagss <32951402+bagyabagss@users.noreply.github.com>
2017-10-21 11:11:06 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece004
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece004 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+      pass
+
+if __name__ == '__main__':
+    main()
+a=12
+if(a<12):
+    print("a is less than 12")
+elif(a>12 and a<20):
+    print("a is between")
+else:
+    print("a is not found")
